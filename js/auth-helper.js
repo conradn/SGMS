b36ac5bd75a04b8602eea7d2297121c68a126a2c
Samuel Teko <samttekoh@gmail.com>
2023-06-06 05:57:33 +0300

    modifications

--- js/auth-helper.js
+++ js/auth-helper.js
@@ -10,3 +10,18 @@ function token() {
 if (token() == null) {
     window.location.href = "login.html";
 }
+
+
+$(document).ready(function () {
+    $.get('sidemenu.html', function (data) {
+        $('#sidemenu').html(data);
+    });
+
+    $.get('topnav.html', function (data) {
+        $('#topnav').html(data);
+        $('#topnav').css({
+            'overflow': 'auto',
+        });
+        $('#user').html(user().name)
+    });
+});
